b8b97d463df4b5a3290cfb680d638d66d5e2c8d6
NamPhungNguyen <namhien12082003@gmail.com>
2024-03-13 20:33:49 +0700

    bai 1

--- lib/main.dart
+++ lib/main.dart
@@ -24,85 +24,26 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home page"),
       ),
-      body: Container(
-        constraints: BoxConstraints.expand(),
+      body:SafeArea(
         child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                  child: Container(
-                    constraints: BoxConstraints.expand(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                            child: Container(
-                              color: Colors.red,
-                              child: Center(child: Text("A")),
-
-                            ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            color: Colors.green,
-                            child: Center(child: Text("B")),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            color: Colors.blueGrey,
-                            child: Center(child: Text("C")),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            color: Colors.blue,
-                            child: Center(child: Text("D")),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-              ),
-              Expanded(
-                  child: Container(
-                    constraints: BoxConstraints.expand(),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                            child: Container(
-                              child: Center(child: Text("A")),
-                              color: Colors.black12,
-                            ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Center(child: Text("B")),
-                            color: Colors.black45,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Center(child: Text("C")),
-                            color: Colors.lightGreenAccent,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Center(child: Text("D")),
-                            color: Colors.teal,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                  )
-              ),
-            ],
+          constraints: BoxConstraints.expand(),
+          color: Colors.teal,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Red"),
+                Text("Green"),
+                Text("Blue"),
+                Text("Orange"),
+                Text("Gray"),
+                Text("Pink"),
+              ],
+            ),
           ),
         ),
+        
       ),
     );
   }
